7914a4bfcfb7e9ce1736be450f38ac4eb91f3685
varrbor <varrbor@ukr.net>
2024-04-11 20:28:19 +0200

    creating basic functionality

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,8 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off'
   }
 };
